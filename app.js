edbc8262b8e6983bbf4ecb184696df10649f037d
dileep <dileepottikunta@gmail.com>
2025-10-17 06:44:28 +0530

    Html File content Will show

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Dileep! 🚀 | 1234567890 ');
-
+  res.sendFile(path.join(__dirname, 'write.html'));
 });
 
 app.listen(PORT, () => {
