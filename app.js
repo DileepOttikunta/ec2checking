c39ff92c8d113a98d3d6b28d988a985affb27147
dileep <dileepottikunta@gmail.com>
2025-10-18 21:36:21 +0530

    Allow all

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const PORT = 3000;
 
 app.use(cors({
-  origin: 'https://courageous-macaron-f7063f.netlify.app/',  // Allow your Angular app
+  origin: '*',  // Allow your Angular app
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
